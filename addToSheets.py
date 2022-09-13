fb6d4354a582314b29068de6d8723d18ce056072
Max Hager <maxhager28@gmail.com>
2022-09-13 18:13:19 +0200

    restart

--- addToSheets.py
+++ addToSheets.py
@@ -38,49 +38,56 @@ def getAllKeys():
     #I only add this row if it is not already in the csv (check with transaction id)
     
 def addTransactionsToCsv():
-    #get all transactions from json
-    #check if transaction id is already in csv
-    #if not add it to csv
-    #else do nothing
+    #load json with transactions
     convert = json.load(open('transactions.json'))
+    #loop over all transactions
     for i in range(len(convert["transactions"]["booked"])):
-        #iter over all transactions
-        #take one transaction - check if id is in csv
+        #get transaction id 
         getCurrentId = convert["transactions"]["booked"][i]["transactionId"]
-        #check if getCurrentId is in csv
-        #get column with transaction ids from csv
+        #get all id's
         getIds = pd.read_csv('transactions.csv', usecols=['transactionId'])
-        #check if getCurrentId is in column
+        #boolean which checks if id is already in csv
         check = getCurrentId in getIds
-        #if check is true do nothing else add transaction to csv
+        #check if bool is true if yes nothing happpens because id is already in csv
         if check == True:
             print('transaction already in csv')
         else:
-            #add following to last line of csv
-            #load csv into df
+            #read transactions as df
             df = pd.read_csv('transactions.csv')
+            #create a second object of the df
             df2 = df
+            #iter over number of columns 
+            counter = 0
             for j in range(len(df.head().columns)):
-                for k in range(len(convert["transactions"]["booked"])):
-                    #add to last line
-                    #first get value with key if available  
-                    val = convert["transactions"]["booked"][k][df.head().columns[j]]
-                    #add to last line of df2 
-                    df2 = df2.append({df.head().columns[k]: val}, ignore_index=True)
-                    #write df2 to transactions.csv
-    write = df2.to_csv('transactions.csv', index=False)
-                
-                    
-                    
-            #set elem at column i to value of convert["transactions"]["booked"][j][key] - but only if key exists
-            #from last line 
-            print(df.head().columns[i])
-            #iter over indexes of df and get with index value from current record the value if it exists
-            #iter over values from header 
-            #get value from value of first header entry
+                counter += 1
+                print(counter)
+                #list with all value from current iter
+                lWithValues = []
+                lWithValuesAll = []
+                #iter over every transaction
+                #print(len(convert["transactions"]["booked"]))
+                #for k in range(len(convert["transactions"]["booked"])):
+                    #get value with key from current column from current transaction
+                try:
+                    val = convert["transactions"]["booked"][i][df.head().columns[j]]
+                    lWithValues.append(val)
+                    #df2.append({df.head().columns[0]: val}, ignore_index=True)
+                    #add value to position k column in dataframe
+                    #write new df to csv
+                    #add this value to exact this position of k - column
+                except KeyError:
+                    lWithValues.append('0')
+            #print(lWithValues)
+            #break
+            counter = 0
+            #lWithValuesAll.append(lWithValues)
+            #lWithValues = []
+    #print(len(lWithValuesAll))
+                    #add on current column current value
+                    #df2 = df2.append({df.head().columns[k]: val}, ignore_index=True)
+    #write to csv
+    df2.to_csv('transactions.csv', index=False)
             
-            first  = df.head().columns[0]
-            print(first)
     
 def test():
     df = pd.read_csv('transactions.csv')
@@ -116,8 +123,7 @@ def createCsv():
 if __name__ == "__main__":
     #createCsv() only run once for init!
     #print(getFirstDict())
-    test()
-    
+    addTransactionsToCsv()    
 
 
 
